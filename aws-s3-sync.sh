6d9fbc5c208b9b05c82c9d95dff5d6d6816e14d9
Xe Iaso <me@xeiaso.net>
2024-11-26 17:09:38 -0500

    make aws s3 sync less spammy
    Signed-off-by: Xe Iaso <me@xeiaso.net>

--- aws-s3-sync.sh
+++ aws-s3-sync.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-aws s3 sync s3://${BUCKET_NAME}/models/${MODEL_NAME} ~/models/${MODEL_NAME}
+aws s3 sync s3://${BUCKET_NAME}/models/${MODEL_NAME} ~/models/${MODEL_NAME} >> aws-s3-sync.log
 python inline-test.py
