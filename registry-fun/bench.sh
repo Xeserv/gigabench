74b2dc8fd03d075b61011b13b94f949801674420
Xe Iaso <me@xeiaso.net>
2025-01-06 16:55:27 -0500

    add docker registry benchmark script
    Signed-off-by: Xe Iaso <me@xeiaso.net>

---
+++ registry-fun/bench.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+TIGRIS_REG_URL="anu-registry.fly.dev"
+R2_REG_URL="r2-registry-production.xe-782.workers.dev"
+IMAGE="xe/x/mimi:latest"
+
+set -euo pipefail
+
+skopeo login -u ${TIGRIS_REG_USERNAME} -p ${TIGRIS_REG_PASSWORD} ${TIGRIS_REG_URL} 2>&1 > /dev/null
+skopeo login -u ${R2_REG_USERNAME} -p ${R2_REG_PASSWORD} ${R2_REG_URL} 2>&1 > /dev/null
+
+mkdir -p data/{tigris,r2}
+
+for i in {1..100}; do
+  tigris_time="$({ time skopeo copy -q docker://${TIGRIS_REG_URL}/${IMAGE} dir:data/tigris; } 2>&1 | head -n2 | tail -n1 | cut -d$'\t' -f2)"
+  echo "tigris,${FLY_REGION},${i},${tigris_time}"
+
+  r2_time="$({ time skopeo copy -q docker://${R2_REG_URL}/${IMAGE} dir:data/r2; } 2>&1 | head -n2 | tail -n1 | cut -d$'\t' -f2)"
+  echo "r2,${FLY_REGION},${i},${r2_time}"
+done
